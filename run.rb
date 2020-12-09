8115c4dca0f430841cb046294c9a3bc5a46727ad
Josh Frank <eurosewusa@gmail.com>
2020-12-09 12:06:52 -0500

    Done

--- run.rb
+++ run.rb
@@ -7,31 +7,56 @@ spicy_foods = [
 ]
 
 def print_spicy_foods(spicy_foods)
-
+  spicy_foods.each do |spicy_food|
+    print "#{spicy_food[:name]} | Heat Level: " 
+    spicy_food[:heat_level].times { print "🌶 " }
+    puts
+  end
 end
 
-def get_names(spicy_foods)
-
+def get_names( spicy_foods )
+  result = []
+  spicy_foods.each do | spicy_food |
+    result << spicy_food[ :name ]
+  end
+  result
 end
 
 def spiciest_foods(spicy_foods)
-
+  spicy_foods.each do |spicy_food|
+    spicy_foods.delete(spicy_food) if spicy_food[:heat_level] < 5
+  end
+  spicy_foods
 end
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine)
-
+  spicy_foods.each do | spicy_food |
+    return spicy_food if spicy_food[ :cuisine ] == cuisine
+  end
 end
 
 # BONUS Deliverables
 def print_spiciest_foods(spicy_foods)
-
+  spicy_foods.each do |spicy_food|
+    if spicy_food[:heat_level] > 5
+      print "#{spicy_food[:name]} | Heat Level: " 
+      spicy_food[:heat_level].times { print "🌶 " }
+      puts
+    end
+  end
 end
 
 def average_heat_level(spicy_foods)
-
+  total = 0
+  spicy_foods.each do |spicy_food|
+    total += spicy_food[:heat_level]
+  end
+  total / spicy_foods.length
 end
+
+puts average_heat_level(spicy_foods)
 
 # Use this to test your methods
 # run "ruby run.rb" and try calling the methods from the console
-binding.pry
-"pls"
+#binding.pry
+#"pls"
